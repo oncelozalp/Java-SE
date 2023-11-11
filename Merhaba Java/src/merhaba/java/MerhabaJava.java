4377ed853c00bfb41b5d25ffa9caa188813b2049
Öncel Özalp <150549252+oncelozalp@users.noreply.github.com>
2023-11-11 23:32:10 +0300

    Java SE ilk program.

---
+++ Merhaba Java/src/merhaba/java/MerhabaJava.java
@@ -0,0 +1,15 @@
+package merhaba.java;
+/**
+ * @author Öncel ÖZALP
+ */
+public class MerhabaJava {
+  
+    public static void main(String[] args) {
+        
+        System.out.println("İlk Proje"); // println yazar ve imleç alt satıra iner. 
+        System.out.print("Merhaba \n");   // print yazar ve imleç orada kalır.  \n de ln görevi görür.
+        System.err.print("JAVA SE "); // Konsola kırmızı hata mesajı şeklinde yazar.
+        
+    }
+    
+}
