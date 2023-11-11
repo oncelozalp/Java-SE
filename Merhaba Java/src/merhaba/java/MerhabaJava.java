260f3617c2d25186126517a26fa8232551560732
Öncel Özalp <150549252+oncelozalp@users.noreply.github.com>
2023-11-11 23:36:54 +0300

    Update MerhabaJava.java

--- Merhaba Java/src/merhaba/java/MerhabaJava.java
+++ Merhaba Java/src/merhaba/java/MerhabaJava.java
@@ -1,14 +1,12 @@
 package merhaba.java;
-/**
- * @author Öncel ÖZALP
- */
+
 public class MerhabaJava {
   
     public static void main(String[] args) {
         
-        System.out.println("İlk Proje"); // println yazar ve imleç alt satıra iner. 
-        System.out.print("Merhaba \n");   // print yazar ve imleç orada kalır.  \n de ln görevi görür.
-        System.err.print("JAVA SE "); // Konsola kırmızı hata mesajı şeklinde yazar.
+        System.out.println("İlk Proje"); // println kullanımı : imlecin bulunduğu satıra ifadeyi yazar ve imleç alt satıra iner. 
+        System.out.print("Merhaba \n");   // print kullanımı : imlecin bulunduğu satıra yazar ve imleç orada kalır.  ( \n kullanımı : imleç alt satıra iner. )
+        System.err.print("JAVA SE "); // err kullanımı: Konsola kırmızı hata mesajı şeklinde yazar.
         
     }
     
